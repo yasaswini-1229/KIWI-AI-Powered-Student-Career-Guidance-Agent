c7e87898cb8ab3347839b5ef1f90687dc9d5acd1
Yasaswini <nee GitHub email>
2026-07-19 17:53:11 +0530

    Fixed password hashing and completed user registration API

--- backend/models/user.py
+++ backend/models/user.py
@@ -6,6 +6,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
